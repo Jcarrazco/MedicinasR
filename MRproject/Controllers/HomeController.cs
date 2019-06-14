b85bd6d37991fcee537baa0c0280dde847d48de5
carrascoja <carrascoja@HGDLAPCARRASCOJ>
2019-06-14 12:40:54 -0500

    Add project files.

---
+++ MRproject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using MRproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DateControl date = new DateControl();
+            ViewBag.Date = date.Datetoday();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MRproject/Models/DateControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MRproject.Models
+{
+    public class DateControl
+    {
+        public string Datetoday()
+        {
+            string days = DateTime.Now.DayOfWeek.ToString();
+            switch (days)
+            {
+                case "Monday":
+                    days = "Lunes";
+                    break;
+                case "Tuesday":
+                    days = "Martes";
+                    break;
+                case "Wednesday":
+                    days = "Miercoles";
+                    break;
+                case "Thursday":
+                    days = "Jueves";
+                    break;
+                case "Friday":
+                    days = "Viernes";
+                    break;
+                case "Saturday":
+                    days = "Sabado";
+                    break;
+                case "Sunday":
+                    days = "Domingo";
+                    break;
+            }
+            string[] Months = new string[] { "Enero", "Febreo", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+            int day = DateTime.Now.Day;
+            int month = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+            //Constructor of strings
+            return days + " " + day + " de " + Months[month-1] + " del " +year;
+        }
+    }
+}
