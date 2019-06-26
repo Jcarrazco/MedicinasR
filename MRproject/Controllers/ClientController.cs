55589071d5fee1275f670dbd11efb4f282edf2c0
Jorge Antonio Carrazco Alvarez <carrazcoja@globalhitss.com>
2019-06-26 16:46:58 -0500

    add forms to get the register info of a client, add a client controller, add three views for client controller, add a javascript file in javascript folder to make a dynamic phone number add and reference the script in layout

---
+++ MRproject/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRproject.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Form1()
+        {
+            return View();
+        }
+
+        public ActionResult Form2()
+        {
+            return View();
+        }
+
+        public ActionResult Form3()
+        {
+            return View();
+        }
+    }
+}
---
+++ MRproject/Javascript/JavaScript.js
@@ -0,0 +1,20 @@
+﻿function addfieldnumber() {
+    var Container = document.getElementById("phones");
+    var nameid = 1;
+    while ($("#Tel" + nameid).length != 0) {
+        nameid++;
+    }
+    if (nameid <= 3) {
+        var input = document.createElement("input");
+        input.type = "text";
+        input.className = "form-control";
+        input.id = "Tel" + nameid;
+        input.placeholder = "Numero telefonico";
+        var label = document.createElement("label");
+        label.htmlFor = "Tel" + nameid;
+        label.innerHTML = "Numero telefono/celular:";
+        Container.appendChild(document.createElement("br"));
+        Container.appendChild(label);
+        Container.appendChild(input);
+    }
+}
